3aab2a1371ba658939318e4f7b3eb8dae29e7d27
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-03-27 19:11:30 +0530

    Task Project Final

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,48 @@ import { Check, X } from 'lucide-react';
 import { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState(''); //task Input State
-  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState(''); // Input Value State
+  const [tasks, setTasks] = useState(() => {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+  }); // Tasks State
 
-  const updateTasks = (e) => {
+  const updateTask = (e) => {
     e.preventDefault();
     if (inputValue) {
       setTasks([{ task: inputValue, isDone: false }, ...tasks]);
-      //settasks((prevTasks) =>[...prevTasks, inputValue])
+      localStorage.setItem(
+        'tasks',
+        JSON.stringify([{ task: inputValue, isDone: false }, ...tasks])
+      );
+      // setTasks((prevTasks) => [...prevTasks, inputValue]);
       setInputValue('');
     }
   };
+
+  const deleteTask = (index) => {
+    const newTasks = [...tasks]; // Copying the tasks array
+    newTasks.splice(index, 1); // Removing the task from the array
+    setTasks(newTasks); // Updating the tasks array
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks]; // Copying the tasks array
+    newTasks[index].isDone = true; // Updating the task status
+    setTasks(newTasks); // Updating the tasks array
+
+    localStorage.setItem('tasks', JSON.stringify(newTasks));
+  };
+
   return (
     <section className="flex justify-center items-center min-h-screen">
-      <section className="p-5 shadow-xl rounded-xl bg-green-100 h-96 w-[90%] max-w-md overflow-y-scroll">
-        <form onSubmit={updateTasks} className="flex justify-between gap-5">
+      <section className="px-5 shadow-xl rounded-xl bg-green-100 h-96 w-[90%] max-w-md overflow-y-auto">
+        <form
+          onSubmit={updateTask}
+          className="py-5 sticky top-0 left-0 right-0 z-10 flex justify-between gap-5 bg-green-100"
+        >
           <input
+            required
             type="text"
             placeholder="Write Your task here.."
             className="px-4 border-2 border-green-600 w-3/4 h-10 rounded"
@@ -26,39 +52,84 @@ function App() {
             }}
             value={inputValue}
           />
-          <button className="bg-green-600 h-10 w-1/4 rounded text-white">
+          <button
+            type="submit"
+            className="bg-green-600 h-10 w-1/4 rounded text-white"
+          >
             Add
           </button>
         </form>
-        <ul className="mt-5 grid gap-2 divide-y divide-gray-400">
-          {tasks.map((task, index) => (
-            <li
-              className={`flex justify-between pb-2 ${
-                task.isDone && 'line-through decoration-gray-900'
-              }`}
-              key={index}
-            >
-              <span>{task.task}</span>
+        {/* Pending Tasks */}
+        {tasks.length > 0 && (
+          <section>
+            <h3 className="font-semibold">Pending Tasks</h3>
+            <ul className="mt-5 grid gap-2 divide-y divide-gray-400">
+              {tasks.map((task, index) => {
+                if (task.isDone !== true) {
+                  return (
+                    <li
+                      className={`flex justify-between pb-2 ${
+                        task.isDone && 'line-through decoration-gray-900'
+                      }`}
+                      key={index}
+                    >
+                      <span>{task.task}</span>
+                      <div className="flex gap-2">
+                        {!task.isDone && (
+                          <Check
+                            size={20}
+                            className="text-gray-500 cursor-pointer hover:text-green-900"
+                            onClick={() => {
+                              completeTask(index);
+                            }}
+                          />
+                        )}
 
-              <div className="flex gap-2">
-                <Check
-                  size={20}
-                  className="text-gray-500 cursor-pointer hover:text-gray-900"
-                  onClick={() => {
-                    const newTasks = [...tasks];
-                    newTasks[index].isDone = true;
-                    setTasks(newTasks);
-                  }}
-                />
+                        <X
+                          size={20}
+                          className="text-gray-500 cursor-pointer hover:text-red-600"
+                          onClick={() => {
+                            deleteTask(index);
+                          }}
+                        />
+                      </div>
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          </section>
+        )}
+        {/* Completed Tasks */}
+        {tasks.length > 0 && (
+          <section className="mt-5">
+            <h3 className="font-semibold">Completed Tasks</h3>
+            <ul className="mt-5 grid gap-2 divide-y divide-gray-400">
+              {tasks.map((task, index) => {
+                if (task.isDone == true) {
+                  return (
+                    <li
+                      className={`flex justify-between pb-2 ${
+                        task.isDone && 'line-through decoration-gray-900'
+                      }`}
+                      key={index}
+                    >
+                      <span>{task.task}</span>
 
-                <X
-                  size={20}
-                  className="text-gray-500 cursor-pointer hover:text-red-600"
-                />
-              </div>
-            </li>
-          ))}
-        </ul>
+                      <X
+                        size={20}
+                        className="text-gray-500 cursor-pointer hover:text-red-600"
+                        onClick={() => {
+                          deleteTask(index);
+                        }}
+                      />
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          </section>
+        )}
       </section>
     </section>
   );
